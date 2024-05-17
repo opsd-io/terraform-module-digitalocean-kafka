1e390b814b79e176715417ab7765456e4931eb78
jpluta <jakub.pluta@zdt.io>
2024-05-17 10:55:19 +0200

    examples

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,6 +1,18 @@
 module "example" {
-  source = "github.com/opsd-io/terraform-module-template?ref=main"
+  source = "github.com/opsd-io/terraform-module-digitalocean-kafka"
 
-  name = "module-test"
-  size = 9000
+  cluster_name     = "example"
+  region           = "nyc1"
+  topics           = ["topic1", "topic2"]
+
+  database_users = {
+    "Donald" = {
+      "topic1" = "consume",
+      "topic2" = "produceconsume"
+    },
+    "Joe" = {
+      "topic1" = "consume",
+      "topic2" = "produce"
+    }
+  }
 }
