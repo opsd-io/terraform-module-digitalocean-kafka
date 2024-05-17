6e87fbccd876f66ea3008cf6190c3d5bef924627
jpluta <jakub.pluta@zdt.io>
2024-05-17 14:55:12 +0200

    examples

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,15 +1,15 @@
 module "example" {
   source = "github.com/opsd-io/terraform-module-digitalocean-kafka"
 
-  cluster_name     = "example"
-  region           = "nyc1"
-  topics           = {
+  cluster_name = "example"
+  region       = "nyc1"
+  topics = {
     "topic1" = {
       replication_factor = 3
       config = {
         retention_ms = 302400000
       }
-    }, 
+    },
     "topic2" = {}
   }
 
